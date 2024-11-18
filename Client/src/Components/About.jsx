e227267ef54258578c886624ceadc7f2bc2837c5
Dhisha09 <146918989+Dhisha09@users.noreply.github.com>
2024-11-18 23:27:20 +0530

    Create About.jsx

---
+++ Client/src/Components/About.jsx
@@ -0,0 +1,111 @@
+                    href="https://www.instagram.com/osamashaikh_786/"
+                    className="fab fa-instagram"
+                  >
+                    <span></span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/" className="fab fa-linkedin">
+                    <span></span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div
+              data-aos="fade-down"
+              data-aos-offset="300"
+              data-aos-easing="ease-in-out"
+              className="about-card"
+            >
+              <div className="img">
+                <img src="https://ik.imagekit.io/bhanu1776/Freelansters/bhanu.jpg?updatedAt=1682608756655" alt="" loading="lazy" />
+              </div>
+              <div className="info">
+                <h3>Bhanu Sunka</h3>
+                <span>Full Stack Developer</span>
+              </div>
+              <ul className="social">
+                <li>
+                  <a
+                    href="https://github.com/Bhanu1776"
+                    className="fab fa-facebook"
+                  >
+                    <span></span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://twitter.com/SunkaBhanu"
+                    className="fab fa-twitter"
+                  >
+                    <span></span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.instagram.com/bhanu_1776/"
+                    className="fab fa-instagram"
+                  >
+                    <span></span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/bhanu-sunka-94a24a21a/"
+                    className="fab fa-linkedin"
+                  >
+                    <span></span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div
+              data-aos="fade-left"
+              data-aos-offset="300"
+              data-aos-easing="ease-in-out"
+              className="about-card"
+            >
+              <div className="img">
+                <img src="https://ik.imagekit.io/bhanu1776/Freelansters/harsh.jpg?updatedAt=1682608753482" alt="" loading="lazy"/>
+              </div>
+              <div className="info">
+                <h3>Harsh Dalvi</h3>
+                <span>Backend Developer</span>
+              </div>
+              <ul className="social">
+                <li>
+                  <a href="/" className="fab fa-facebook">
+                    <span></span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/" className="fab fa-twitter">
+                    <span></span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/https://www.instagram.com/harsh_97"
+                    className="fab fa-instagram"
+                  >
+                    <span></span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/" className="fab fa-linkedin">
+                    <span></span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </main>
+      </Wrapper>
+      {/* //? ------------------------------------------------------------------------------ */}
+    </>
+  );
+};
+
+export default About;
